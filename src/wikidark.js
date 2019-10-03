e4a0b8b4e84857be2a67825e7cf6f0e3b25e21e9
Rebecca Peck <me@rebeccapeck.org>
2019-10-02 21:40:55 -0500

    Round of various small fixes and improvements.

--- src/wikidark.js
+++ src/wikidark.js
@@ -1,22 +1,31 @@
 $(document).ready(function() {
 
-    media = window.matchMedia('(prefers-color-scheme: dark)');
+    var mediaMatch = '(prefers-color-scheme: dark)';
 
+    function matchFunc() {
+        $(".mw-parser-output")
+            .find("*")
+            .each(function(i, elem) { 
+                if(elem.style.borderWidth) { 
+                    elem.style.borderColor = "red"
+                }
+                
+                if (elem.style.backgroundColor) {
+                    elem.style.backgroundColor = "inherit";
+                }
+            });
+    }
+
+    media = window.matchMedia(mediaMatch);
     media.addListener(function() {
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            $(".mw-parser-output")
-                .find("*")
-                .each(function(i, elem) { 
-                    if(elem.style.borderWidth) { 
-                        elem.style.borderColor = "red"
-                    }
-                    
-                    if (elem.style.backgroundColor) {
-                        elem.style.backgroundColor = "black";
-                    }
-                });
+        if (window.matchMedia(mediaMatch).matches) {
+            matchFunc();
         } else {
             window.location.reload();
         }
     });
+
+    if (window.matchMedia(mediaMatch).matches) {
+        matchFunc();
+    }
 });
